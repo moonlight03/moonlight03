e0c0dc31803424902276b6996ab9461ae9055539
moonlight03 <1739781578@qq.com>
2024-04-29 17:05:57 +0800

    123123

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://moonlight03.github.io",
+  site: "https://moonlight03.github.io/moonlight03",
   base: "/moonlight03",
   outDir: './docs',
   integrations: [
